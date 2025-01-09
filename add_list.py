1a312976ea80328b7f3493a55c3e2c00ddf7e2e9
Андрей <andrey.gazizov.2012@mail.ru>
2025-01-09 19:59:29 +0700

    Add files via upload

--- add_list.py
+++ add_list.py
@@ -2,6 +2,23 @@ title1 = input("Введите заголовок заметки:")
 title2 = input("Введите заголовок заметки:")
 title3 = input ("Введите заголовок заметки:")
 
-title_list = [title1, title2, title3]
+title = [title1, title2, title3]
 
-print(title_list)
+print("Превый заголовок:", title[0])
+print("Второй заголовок:", title[1])
+print("Третий заголовок:", title[2])
+
+username = ("Имя пользователя:")
+title = ("Заголовок заметки:")
+content = ("Описание заметки:")
+status = ("Статус заметки:")
+created_date = ("Дата создания заметки (в формате дд.мм.гг.):")
+issue_date = ("Дата истечения заметки (в формате дд.мм.гг.):")
+
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
+
